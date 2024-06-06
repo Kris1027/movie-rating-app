fca698e89731d309fef902e6bc430b762139b74d
Kris1027 <obarzanek.work@gmail.com>
2024-06-06 15:23:46 +0200

    build getSeries for fetching series and series component and render on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Movies } from "./components/movies";
+import { Series } from "./components/series";
 
 export default function App() {
   return (
     <div>
       <h1>Home Page</h1>
       <Movies />
+      <Series />
     </div>
   );
 }
