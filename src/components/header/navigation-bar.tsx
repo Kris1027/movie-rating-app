5e55762a49c6219bb21692398dff117acac7d140
Kris1027 <obarzanek.work@gmail.com>
2024-06-08 00:11:42 +0200

    build dark mode

--- src/components/header/navigation-bar.tsx
+++ src/components/header/navigation-bar.tsx
@@ -1,11 +1,13 @@
 import { Logo } from "./logo";
 import { Menu } from "./menu";
+import { ModeToggle } from "./mode-toggle";
 
 export function NavigationBar() {
   return (
     <header className="flex justify-between p-4 text-primary bg-primary-foreground">
       <Logo />
       <Menu />
+      <ModeToggle />
     </header>
   );
 }
