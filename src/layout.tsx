35861dd19739b0958d91a802bc0608d47953c247
Kris1027 <obarzanek.work@gmail.com>
2024-06-07 12:44:20 +0200

    build and render footer component

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,7 +1,10 @@
+import { Footer } from "./components/footer";
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <main className="p-4">{children}</main>
+      <Footer />
     </>
   );
 }
