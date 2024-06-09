da1a39a202b93c28553b74f542b22b98c4e47978
Kris1027 <obarzanek.work@gmail.com>
2024-06-09 18:39:46 +0200

    build one card for movies and series

---
+++ src/components/content-card.tsx
@@ -0,0 +1,43 @@
+import { contentTypes, movieProps, seriesProps } from "@/types/content-types";
+import { Card, CardContent, CardHeader } from "./ui/card";
+import { Heading } from "./ui/heading";
+import { Calendar, Star } from "lucide-react";
+
+export function ContentCart({
+  c,
+  contentType,
+}: {
+  c: movieProps | seriesProps;
+  contentType: contentTypes;
+}) {
+  return (
+    <Card>
+      <CardHeader>
+        <img
+          src={`https://image.tmdb.org/t/p/w500${c.poster_path}`}
+          alt={
+            contentType === "movies"
+              ? (c as movieProps).title
+              : (c as seriesProps).name
+          }
+        />
+        <Heading>
+          {contentType === "movies"
+            ? (c as movieProps).title
+            : (c as seriesProps).name}
+        </Heading>
+      </CardHeader>
+      <CardContent>
+        <p className="flex items-center gap-1">
+          <Calendar />{" "}
+          {contentType === "movies"
+            ? (c as movieProps).release_date
+            : (c as seriesProps).first_air_date}
+        </p>
+        <p className="flex items-center gap-1 font-bold">
+          <Star color="#facc15" /> {c.vote_average.toFixed(2)}
+        </p>
+      </CardContent>
+    </Card>
+  );
+}
--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,13 +1,6 @@
+import { ContentCart } from "@/components/content-card";
 import { ContentSwitcher } from "@/components/content-switcher";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Heading } from "@/components/ui/heading";
-import {
-  contentTypes,
-  dataProps,
-  movieProps,
-  seriesProps,
-} from "@/types/content-types";
-import { Calendar, Star } from "lucide-react";
+import { contentTypes, dataProps } from "@/types/content-types";
 import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
@@ -28,34 +21,7 @@ export function HomePage() {
       />
       <div className="grid grid-cols-3 gap-4 text-center p-4">
         {content.map((c) => (
-          <Card key={c.id}>
-            <CardHeader>
-              <img
-                src={`https://image.tmdb.org/t/p/w500${c.poster_path}`}
-                alt={
-                  contentType === "movies"
-                    ? (c as movieProps).title
-                    : (c as seriesProps).name
-                }
-              />
-              <Heading>
-                {contentType === "movies"
-                  ? (c as movieProps).title
-                  : (c as seriesProps).name}
-              </Heading>
-            </CardHeader>
-            <CardContent>
-              <p className="flex items-center gap-1">
-                <Calendar />{" "}
-                {contentType === "movies"
-                  ? (c as movieProps).release_date
-                  : (c as seriesProps).first_air_date}
-              </p>
-              <p className="flex items-center gap-1 font-bold">
-                <Star color="#facc15" /> {c.vote_average.toFixed(2)}
-              </p>
-            </CardContent>
-          </Card>
+          <ContentCart key={c.id} c={c} contentType={contentType} />
         ))}
       </div>
     </div>
