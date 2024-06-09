a1c9d2769206617505c0ee422d5597b2c677e531
Kris1027 <obarzanek.work@gmail.com>
2024-06-09 18:58:25 +0200

    change folder name from header to navigation

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "@/providers/theme-provider";
 import { Outlet } from "react-router-dom";
-import { NavigationBar } from "./header/navigation-bar";
+import { NavigationBar } from "./navigation/navigation-bar";
 import { Footer } from "./footer";
 
 export function Layout() {
