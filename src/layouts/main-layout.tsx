ad5fdc53289264bf1fd4b192ff78d5149f8d9320
Kris1027 <obarzanek.work@gmail.com>
2024-06-14 01:17:00 +0200

    fix not good import

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "@/providers/theme-provider";
 import { Outlet } from "react-router-dom";
 import { NavigationBar } from "../components/navigation/navigation-bar";
 import { Footer } from "../components/footer";
-import { Wrapper } from "@/components/wrapper";
+import { Wrapper } from "../components/wrapper";
 
 export function MainLayout() {
   return (
