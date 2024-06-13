4e1e16588b067f63373a27cf38a4b792b11d29a7
Kris1027 <obarzanek.work@gmail.com>
2024-06-13 17:37:34 +0200

    add access token

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,3 +3,5 @@ export const DATA_URL = "https://api.themoviedb.org/3";
 export const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 
 export const API_KEY = `${import.meta.env.VITE_API_KEY}`;
+
+export const ACCESS_TOKEN = `${import.meta.env.VITE_ACCESS_TOKEN}`;
