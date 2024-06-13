ea49a002b72ab091d3a7077d4976decad5a8251a
Kris1027 <obarzanek.work@gmail.com>
2024-06-14 01:19:11 +0200

    delete wrapper component

--- src/components/Wrapper.tsx
+++
@@ -1,3 +0,0 @@
-export function Wrapper({ children }: { children: React.ReactNode }) {
-  return <main className="min-h-screen p-4">{children}</main>;
-}
--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -2,15 +2,12 @@ import { ThemeProvider } from "@/providers/theme-provider";
 import { Outlet } from "react-router-dom";
 import { NavigationBar } from "../components/navigation/navigation-bar";
 import { Footer } from "../components/footer";
-import { Wrapper } from "../components/wrapper";
 
 export function MainLayout() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <NavigationBar />
-      <Wrapper>
-        <Outlet />
-      </Wrapper>
+      <Outlet />
       <Footer />
     </ThemeProvider>
   );
