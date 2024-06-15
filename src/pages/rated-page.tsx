14d234ef23aad915d841a0b8e880fde65069b85d
Kris1027 <obarzanek.work@gmail.com>
2024-06-15 19:26:51 +0200

    add style for rated page and works with content switcher

--- src/pages/rated-page.tsx
+++ src/pages/rated-page.tsx
@@ -5,6 +5,7 @@ import { fetchRated } from "@/api/fetch-rated";
 import { Button } from "@/components/ui/button";
 import { RatedContentTypeProps } from "@/types/data-types";
 import { MediaList } from "@/components/media-list";
+import { Heading } from "@/components/ui/heading";
 
 export function RatedPage() {
   const [contentType, setContentType] = useState<RatedContentTypeProps>(
@@ -30,32 +31,47 @@ export function RatedPage() {
   if (isError) return <p className="text-destructive">Error: {isError}</p>;
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Rated Page</h1>
+    <main className="p-4 flex flex-col items-center">
       <div className="my-4">
         <Button
-          variant="default"
+          variant={contentType === "movies" ? "default" : "secondary"}
           onClick={() => handleToggle(RatedContentTypeProps.movies)}
         >
-          Fetch Movies
+          Rated Movies
         </Button>
         <Button
-          variant="default"
+          variant={contentType === "tv" ? "default" : "secondary"}
           onClick={() => handleToggle(RatedContentTypeProps.tv)}
         >
-          Fetch TV Shows
+          Rated TV Shows
         </Button>
       </div>
-      {isLoading && <p>Loading...</p>}
-      {isError && <p className="text-destructive">Error: {isError}</p>}
-      {data && (
-        <div>
-          <h2 className="text-2xl font-bold">
-            {contentType === "movies" ? "Popular Movies" : "Popular TV Shows"}
-          </h2>
-          <MediaList data={data} contentType={contentType} />
-        </div>
-      )}
-    </div>
+      <div className="flex flex-col items-center text-center">
+        {contentType === "movies" ? (
+          <>
+            <Heading>Welcome to Your Rated Movies</Heading>
+            <p className="pb-4">
+              Discover the films you have rated and see how they stack up
+              against your personal favorites. Dive into your own curated
+              collection of movies and find out which ones have earned your
+              highest praise. Enjoy revisiting your top-rated films and explore
+              new recommendations based on your ratings
+            </p>
+          </>
+        ) : (
+          <>
+            <Heading>Welcome to Your Rated TV Shows</Heading>
+            <p className="pb-4">
+              Explore the TV shows you've rated and see which ones stand out as
+              your top picks. Dive into your personalized list of series and
+              discover which shows have left a lasting impression. Enjoy
+              revisiting your highest-rated TV shows and uncover new
+              recommendations tailored to your viewing preferences
+            </p>
+          </>
+        )}
+      </div>
+      <MediaList data={data} contentType={contentType} />
+    </main>
   );
 }
