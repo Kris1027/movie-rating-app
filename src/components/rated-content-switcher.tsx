04558e91f62021f2d5ba530d4f609b4b9ebf6cde
Kris1027 <obarzanek.work@gmail.com>
2024-06-16 02:00:12 +0200

    build content switcher for rated

---
+++ src/components/rated-content-switcher.tsx
@@ -0,0 +1,55 @@
+import { RatedContentTypeProps } from "@/types/data-types";
+import { Button } from "./ui/button";
+import { Heading } from "./ui/heading";
+
+export function RatedContentSwitcher({
+  handleToggle,
+  contentType,
+}: {
+  handleToggle: (contentType: RatedContentTypeProps) => void;
+  contentType: RatedContentTypeProps;
+}) {
+  return (
+    <>
+      <div className="my-4">
+        <Button
+          variant={contentType === "movies" ? "default" : "secondary"}
+          onClick={() => handleToggle(RatedContentTypeProps.movies)}
+        >
+          Rated Movies
+        </Button>
+        <Button
+          variant={contentType === "tv" ? "default" : "secondary"}
+          onClick={() => handleToggle(RatedContentTypeProps.tv)}
+        >
+          Rated TV Shows
+        </Button>
+      </div>
+      <div className="flex flex-col items-center text-center">
+        {contentType === "movies" ? (
+          <>
+            <Heading>Welcome to Your Rated Movies</Heading>
+            <p className="pb-4">
+              Discover the films you have rated and see how they stack up
+              against your personal favorites. Dive into your own curated
+              collection of movies and find out which ones have earned your
+              highest praise. Enjoy revisiting your top-rated films and explore
+              new recommendations based on your ratings
+            </p>
+          </>
+        ) : (
+          <>
+            <Heading>Welcome to Your Rated TV Shows</Heading>
+            <p className="pb-4">
+              Explore the TV shows you've rated and see which ones stand out as
+              your top picks. Dive into your personalized list of series and
+              discover which shows have left a lasting impression. Enjoy
+              revisiting your highest-rated TV shows and uncover new
+              recommendations tailored to your viewing preferences
+            </p>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
--- src/pages/rated-page.tsx
+++ src/pages/rated-page.tsx
@@ -2,10 +2,9 @@ import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 import { fetchRated } from "@/api/fetch-rated";
-import { Button } from "@/components/ui/button";
 import { RatedContentTypeProps } from "@/types/data-types";
 import { MediaList } from "@/components/media-list";
-import { Heading } from "@/components/ui/heading";
+import { RatedContentSwitcher } from "@/components/rated-content-switcher";
 
 export function RatedPage() {
   const [contentType, setContentType] = useState<RatedContentTypeProps>(
@@ -32,45 +31,10 @@ export function RatedPage() {
 
   return (
     <main className="p-4 flex flex-col items-center">
-      <div className="my-4">
-        <Button
-          variant={contentType === "movies" ? "default" : "secondary"}
-          onClick={() => handleToggle(RatedContentTypeProps.movies)}
-        >
-          Rated Movies
-        </Button>
-        <Button
-          variant={contentType === "tv" ? "default" : "secondary"}
-          onClick={() => handleToggle(RatedContentTypeProps.tv)}
-        >
-          Rated TV Shows
-        </Button>
-      </div>
-      <div className="flex flex-col items-center text-center">
-        {contentType === "movies" ? (
-          <>
-            <Heading>Welcome to Your Rated Movies</Heading>
-            <p className="pb-4">
-              Discover the films you have rated and see how they stack up
-              against your personal favorites. Dive into your own curated
-              collection of movies and find out which ones have earned your
-              highest praise. Enjoy revisiting your top-rated films and explore
-              new recommendations based on your ratings
-            </p>
-          </>
-        ) : (
-          <>
-            <Heading>Welcome to Your Rated TV Shows</Heading>
-            <p className="pb-4">
-              Explore the TV shows you've rated and see which ones stand out as
-              your top picks. Dive into your personalized list of series and
-              discover which shows have left a lasting impression. Enjoy
-              revisiting your highest-rated TV shows and uncover new
-              recommendations tailored to your viewing preferences
-            </p>
-          </>
-        )}
-      </div>
+      <RatedContentSwitcher
+        handleToggle={handleToggle}
+        contentType={contentType}
+      />
       <MediaList data={data} contentType={contentType} />
     </main>
   );
