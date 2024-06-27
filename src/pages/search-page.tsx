577badaad04665f1a7302419152d75bb82fe04e5
Kris1027 <obarzanek.work@gmail.com>
2024-06-27 18:01:55 +0200

    add input from shadcn and build new state for search results

--- src/pages/search-page.tsx
+++ src/pages/search-page.tsx
@@ -1,5 +1,6 @@
 import { searchData } from "@/api/search-data";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { isMovie } from "@/lib/is-movie";
 import { ItemProps } from "@/types/data-types";
 import { useMutation } from "@tanstack/react-query";
@@ -7,15 +8,23 @@ import { useState } from "react";
 
 export function SearchPage() {
   const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState<ItemProps[] | undefined>(
+    undefined
+  );
 
   const { mutate, data, isPending, isError } = useMutation({
     mutationFn: () => searchData(query),
     mutationKey: ["search", query],
+    onSuccess: (data) => {
+      setSearchResults(data.results);
+      setQuery("");
+    },
   });
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     mutate();
+    setQuery("");
   };
 
   const isLoggedIn = Boolean(localStorage.getItem("guest_session_id"));
@@ -24,26 +33,32 @@ export function SearchPage() {
     return <p>You must be logged in to view this page</p>;
   }
 
+  console.log(data);
+
   return (
-    <main>
-      <form className="text-black" onSubmit={handleSubmit}>
-        <input
+    <main className="p-4 flex flex-col items-center">
+      <form className="text-black flex gap-4 pb-4" onSubmit={handleSubmit}>
+        <Input
+          className="text-primary"
           type="search"
           onChange={(e) => setQuery(e.target.value)}
           value={query}
         />
-        <Button type="submit" disabled={isPending}>
+        <Button type="submit" disabled={isPending} variant="destructive">
           {isPending ? "Searching..." : "Search"}
         </Button>
       </form>
 
       {isError && <p>An error occurred. Please try again.</p>}
 
-      {data && (
+      {searchResults && (
         <div>
-          {data?.results?.map((item: ItemProps) => (
-            <p key={item.id}>{isMovie(item) ? item.title : item.name}</p>
+          {searchResults.map((item: ItemProps) => (
+            <div key={item.id}>
+              <p>{isMovie(item) ? item.title : item.name}</p>
+            </div>
           ))}
+          {searchResults.length === 0 && <p>No results found</p>}
         </div>
       )}
     </main>
