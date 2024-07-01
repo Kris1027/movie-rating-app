d6ec9cba0f0ed3fc35b3097f7b4ea9ba74d13c3d
Kris1027 <obarzanek.work@gmail.com>
2024-07-01 22:50:22 +0200

    fix nested paragraph bug

--- src/components/rated-content-switcher.tsx
+++ src/components/rated-content-switcher.tsx
@@ -40,26 +40,34 @@ export function RatedContentSwitcher({
         {contentType === RatedContentTypeProps.movies ? (
           <>
             {hasRatedMovies && <Heading>Welcome to Your Rated Movies</Heading>}
-            <p className="pb-4">
-              {hasRatedMovies ? (
-                "Discover the films you have rated and see how they stack up against your personal favorites. Dive into your own curated collection of movies and find out which ones have earned your highest praise. Enjoy revisiting your top-rated films and explore new recommendations based on your ratings"
-              ) : (
-                <NoRatedContent contentType={RatedContentTypeProps.movies} />
-              )}
-            </p>
+            {hasRatedMovies ? (
+              <p className="pb-4">
+                Discover the films you have rated and see how they stack up
+                against your personal favorites. Dive into your own curated
+                collection of movies and find out which ones have earned your
+                highest praise. Enjoy revisiting your top-rated films and
+                explore new recommendations based on your ratings
+              </p>
+            ) : (
+              <NoRatedContent contentType={RatedContentTypeProps.movies} />
+            )}
           </>
         ) : (
           <>
             {hasRatedTvShows && (
               <Heading>Welcome to Your Rated TV Shows</Heading>
             )}
-            <p className="pb-4">
-              {hasRatedTvShows ? (
-                "Explore the TV shows you've rated and see which ones stand out as your top picks. Dive into your personalized list of series and discover which shows have left a lasting impression. Enjoy revisiting your highest-rated TV shows and uncover new recommendations tailored to your viewing preferences"
-              ) : (
-                <NoRatedContent contentType={RatedContentTypeProps.tv} />
-              )}
-            </p>
+            {hasRatedTvShows ? (
+              <p className="pb-4">
+                Explore the TV shows you've rated and see which ones stand out
+                as your top picks. Dive into your personalized list of series
+                and discover which shows have left a lasting impression. Enjoy
+                revisiting your highest-rated TV shows and uncover new
+                recommendations tailored to your viewing preferences
+              </p>
+            ) : (
+              <NoRatedContent contentType={RatedContentTypeProps.tv} />
+            )}
           </>
         )}
       </div>
