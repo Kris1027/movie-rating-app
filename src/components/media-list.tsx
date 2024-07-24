cd626dafd36b90cfd7a1241d683ab8a5c99125f2
Kris1027 <obarzanek.work@gmail.com>
2024-07-25 00:11:02 +0200

    responsive media list

--- src/components/media-list.tsx
+++ src/components/media-list.tsx
@@ -15,7 +15,7 @@ export function MediaList({
    contentType: ContentTypeProps | RatedContentTypeProps;
 }) {
    return (
-      <div className='grid grid-cols-4 gap-4'>
+      <div className='grid grid-cols-1 gap-2 sm:gap-4 md:gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
          {data.results.map((item: ItemProps) => (
             <MediaCard key={item.id} item={item} contentType={contentType} />
          ))}
